c361ff0437b2d34e9cdeb104492cbf8d0d4c4908
Henry <193541T@mymail.nyp.edu.sg>
2021-02-04 21:33:38 +0800

    Update Source.cpp

--- Simple_TCP_Echo_Server/Source.cpp
+++ Simple_TCP_Echo_Server/Source.cpp
@@ -54,25 +54,28 @@ int main(const int, const char* const* const){
     }
 
     (void)printf("[TCP Echo Server] Waiting for client to connect...\n");
-
-    clientSocket = accept(serverSocket, (SOCKADDR*)&clientAddress, &sizeOfClientAddress);
-    if(clientSocket == INVALID_SOCKET){
-        (void)printf("accept failed with error %ld\n", WSAGetLastError());
-        (void)closesocket(serverSocket);
-        (void)WSACleanup();
-        return 1;
-    } else{
-        (void)system("cls");
-        (void)printf("[TCP Echo Server] Client (%d.%d.%d.%d: %d) connected...\n\n",
-            clientAddress.sin_addr.S_un.S_un_b.s_b1,
-            clientAddress.sin_addr.S_un.S_un_b.s_b2,
-            clientAddress.sin_addr.S_un.S_un_b.s_b3,
-            clientAddress.sin_addr.S_un.S_un_b.s_b4,
-            ntohs(clientAddress.sin_port)
-        );
-    }
+    result = SOCKET_ERROR;
 
     for(;;){
+        if(result == SOCKET_ERROR){
+            clientSocket = accept(serverSocket, (SOCKADDR*)&clientAddress, &sizeOfClientAddress);
+            if(clientSocket == INVALID_SOCKET){
+                (void)printf("accept failed with error %ld\n", WSAGetLastError());
+                (void)closesocket(serverSocket);
+                (void)WSACleanup();
+                return 1;
+            } else{
+                (void)system("cls");
+                (void)printf("[TCP Echo Server] Client (%d.%d.%d.%d: %d) connected...\n\n",
+                    clientAddress.sin_addr.S_un.S_un_b.s_b1,
+                    clientAddress.sin_addr.S_un.S_un_b.s_b2,
+                    clientAddress.sin_addr.S_un.S_un_b.s_b3,
+                    clientAddress.sin_addr.S_un.S_un_b.s_b4,
+                    ntohs(clientAddress.sin_port)
+                );
+            }
+        }
+
         memset(msgBuffer, '\0', bufferLen);
         result = recv(clientSocket, msgBuffer, bufferLen, 0);
         //result = recv(clientSocket, msgBuffer, 2, 0);
@@ -87,14 +90,33 @@ int main(const int, const char* const* const){
                 ntohs(clientAddress.sin_port),
                 result
             );
+        } else if(result == SOCKET_ERROR){
+            (void)system("cls");
+            (void)printf("[TCP Echo Server] Client (%d.%d.%d.%d: %d) disconnected...\n\n",
+                clientAddress.sin_addr.S_un.S_un_b.s_b1,
+                clientAddress.sin_addr.S_un.S_un_b.s_b2,
+                clientAddress.sin_addr.S_un.S_un_b.s_b3,
+                clientAddress.sin_addr.S_un.S_un_b.s_b4,
+                ntohs(clientAddress.sin_port)
+            );
+            continue;
         } else{
             result == 0 ? (void)printf("Connection closed\n") : (void)printf("recv failed with error %d\n", WSAGetLastError());
         }
 
         result = send(clientSocket, msgBuffer, result, 0);
         if(result == SOCKET_ERROR){
-            (void)printf("send failed with error %d\n", WSAGetLastError());
-            break;
+            //(void)printf("send failed with error %d\n", WSAGetLastError());
+            //break;
+            (void)system("cls");
+            (void)printf("[TCP Echo Server] Client (%d.%d.%d.%d: %d) disconnected...\n\n",
+                clientAddress.sin_addr.S_un.S_un_b.s_b1,
+                clientAddress.sin_addr.S_un.S_un_b.s_b2,
+                clientAddress.sin_addr.S_un.S_un_b.s_b3,
+                clientAddress.sin_addr.S_un.S_un_b.s_b4,
+                ntohs(clientAddress.sin_port)
+            );
+            continue;
         } else{
             (void)printf("Sent \"%s\" back (to %d.%d.%d.%d: %d, bytes sent: %d)\n\n",
                 msgBuffer,
