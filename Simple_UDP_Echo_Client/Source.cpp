9bf419bd2e36702981515a6a6a1d7f59b6ff990a
Henry <193541T@mymail.nyp.edu.sg>
2021-02-04 16:02:49 +0800

    Update Source.cpp

--- Simple_UDP_Echo_Client/Source.cpp
+++ Simple_UDP_Echo_Client/Source.cpp
@@ -71,14 +71,14 @@ int main(const int, const char* const* const){
             break;
         }
 
-        printf("Read bytes = %d, Message = [%s], from %d.%d.%d.%d:%d\n",
-            result,
+        printf("\"%s\" (from %d.%d.%d.%d: %d, read bytes: %d)\n\n",
             msgBuffer,
             senderAddress.sin_addr.S_un.S_un_b.s_b1,
             senderAddress.sin_addr.S_un.S_un_b.s_b2,
             senderAddress.sin_addr.S_un.S_un_b.s_b3,
             senderAddress.sin_addr.S_un.S_un_b.s_b4,
-            ntohs(senderAddress.sin_port)
+            ntohs(senderAddress.sin_port),
+            result
         );
     }
 
