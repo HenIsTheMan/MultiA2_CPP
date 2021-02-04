98a9fad6ab1fa9d97b8538e1b7b3242e3fd68ac8
Henry <193541T@mymail.nyp.edu.sg>
2021-02-04 16:07:08 +0800

    Update Source.cpp

--- Simple_UDP_Echo_Client/Source.cpp
+++ Simple_UDP_Echo_Client/Source.cpp
@@ -17,7 +17,7 @@ int main(const int, const char* const* const){
     const int portNumber = 9876;
 
     WSADATA wsaData;
-    SOCKET receiverSocket; //For receiving datagrams
+    SOCKET mySocket;
     SOCKADDR_IN receiverAddress{};
     SOCKADDR_IN senderAddress{};
     char msgBuffer[defaultBufferLen]{};
@@ -31,19 +31,18 @@ int main(const int, const char* const* const){
         return 1;
     }
 
-    receiverSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if(receiverSocket == INVALID_SOCKET){
+    mySocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if(mySocket == INVALID_SOCKET){
         (void)printf("socket failed with error %d\n", WSAGetLastError());
         return 1;
     }
 
-    ///Set up the receiverAddress structure with the IP address of the receiver (in this example case "192.168.1.1") ??
     receiverAddress.sin_family = AF_INET;
     receiverAddress.sin_port = htons(portNumber);
     receiverAddress.sin_addr.s_addr = inet_addr(serverIPAddress);
 
     for(;;){
-        (void)printf("Enter msgs: ");
+        (void)printf("[UDP Echo Client] Enter msg: ");
 
         int i = 0;
         while(i < defaultBufferLen - 1){
@@ -56,13 +55,13 @@ int main(const int, const char* const* const){
         }
         msgSize = i;
 
-        result = sendto(receiverSocket, msgBuffer, msgSize, 0, (SOCKADDR*)&receiverAddress, sizeof(receiverAddress)); //Send a datagram
+        result = sendto(mySocket, msgBuffer, msgSize, 0, (SOCKADDR*)&receiverAddress, sizeof(receiverAddress)); //Send a datagram
         if(result == SOCKET_ERROR){
             (void)printf("sendto failed with error: %d\n", WSAGetLastError());
             break;
         }
 
-        result = recvfrom(receiverSocket, msgBuffer, defaultBufferLen, 0, (SOCKADDR*)&senderAddress, &sizeOfSenderAddress); //Receive datagrams
+        result = recvfrom(mySocket, msgBuffer, defaultBufferLen, 0, (SOCKADDR*)&senderAddress, &sizeOfSenderAddress); //Receive datagrams
         if(result == SOCKET_ERROR){
             (void)printf("recvfrom failed with error %d\n", WSAGetLastError());
             break;
@@ -82,7 +81,7 @@ int main(const int, const char* const* const){
         );
     }
 
-    result = closesocket(receiverSocket);
+    result = closesocket(mySocket);
     if(result == SOCKET_ERROR){
         printf("closesocket failed with error: %d\n", WSAGetLastError());
         return 1;
