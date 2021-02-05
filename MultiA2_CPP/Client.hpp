3a4f5c48bf8afed2196891bd6fd2debf2a1202cd
Henry <193541T@mymail.nyp.edu.sg>
2021-02-06 01:18:17 +0800

    Added Client.hpp and Server.hpp

---
+++ MultiA2_CPP/Client.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Core.hpp"
+
+struct Client final{ //Passive DS (PDS)/Plain Old DS (PODS)/Plain Old Data (POD)
+	_6_DEFAULTED(Client)
+
+
+};
---
+++ MultiA2_CPP/Server.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Core.hpp"
+
+struct Server final{ //Passive DS (PDS)/Plain Old DS (PODS)/Plain Old Data (POD)
+	_6_DEFAULTED(Server)
+
+
+};
