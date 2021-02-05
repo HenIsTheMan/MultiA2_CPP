8478151e421d0e2b477b38396fbdd6c69757e261
Henry <193541T@mymail.nyp.edu.sg>
2021-02-06 01:10:16 +0800

    go go

---
+++ MultiA2_CPP/Core.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef _6_DEFAULTED
+#define _6_DEFAULTED(...) \
+		__VA_ARGS__() = default; \
+		\
+		__VA_ARGS__(const __VA_ARGS__&) = default; \
+		__VA_ARGS__(__VA_ARGS__&&) noexcept = default; \
+		\
+		__VA_ARGS__& operator=(const __VA_ARGS__&) = default; \
+		__VA_ARGS__& operator=(__VA_ARGS__&&) noexcept = default; \
+		\
+		~__VA_ARGS__() = default;
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <winsock2.h>
+
+#pragma comment(lib, "Ws2_32.lib")
