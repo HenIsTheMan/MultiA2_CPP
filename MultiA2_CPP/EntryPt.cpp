138b0a6c31d7c213539b047a5a9178becbed0318
Henry <193541T@mymail.nyp.edu.sg>
2021-02-08 06:36:57 +0800

    Update EntryPt.cpp

--- MultiA2_CPP/EntryPt.cpp
+++ MultiA2_CPP/EntryPt.cpp
@@ -69,7 +69,8 @@ int main(const int argc, const char* const* const argv){
 		} while(portNumbers.find(val) != portNumbers.end());
 
         server->Init({
-            val
+            val,
+			ProtocolType::UDP
         });
 		portNumbers.insert(val); 
     }
